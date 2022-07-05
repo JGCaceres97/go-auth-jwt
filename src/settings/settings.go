47e4d43049e76f9c1c48346b35b3c286229ddf36
Joseph Cáceres <jgcaceres97@gmail.com>
2022-07-04 22:23:27 -0600

    Tipo de dato cambiando para el puerto

--- src/settings/settings.go
+++ src/settings/settings.go
@@ -14,7 +14,7 @@ var DB *Database
 
 type Database struct {
 	Host     *string `yaml:"host"`
-	Port     *int    `yaml:"port"`
+	Port     *uint16 `yaml:"port"`
 	User     *string `yaml:"user"`
 	Password *string `yaml:"password"`
 	Name     *string `yaml:"name"`
